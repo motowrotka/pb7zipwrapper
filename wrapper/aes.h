99fd8de0b5296a4db93c00892c1903dd8571ca9f
motowrotka <motowrotka@gmail.com>
2026-03-08 19:57:40 +0100

    Update aes.h

--- wrapper/aes.h
+++ wrapper/aes.h
@@ -11,6 +11,7 @@ void aes256_encrypt_block(uint8_t* block);
 void aes256_decrypt_block(uint8_t* block);
 void aes256_cbc_encrypt(uint8_t* data, size_t len, const uint8_t* iv);
 void aes256_cbc_decrypt(uint8_t* data, size_t len, const uint8_t* iv);
+int aes256_selftest(void);
 
 #ifdef __cplusplus
 }
