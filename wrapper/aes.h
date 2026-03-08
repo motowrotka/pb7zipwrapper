88c808d4dc8226eefc9176e5f01cea320810d926
motowrotka <motowrotka@gmail.com>
2026-03-08 16:29:49 +0100

    Uncomment AES decryption function declarations

--- wrapper/aes.h
+++ wrapper/aes.h
@@ -8,10 +8,10 @@ extern "C" {
 
 void aes256_init(const uint8_t* key);
 void aes256_encrypt_block(uint8_t* block);
-void aes256_decrypt_block(uint8_t* block);      // na razie pusta implementacja
+void aes256_decrypt_block(uint8_t* block);
 
 void aes256_cbc_encrypt(uint8_t* data, size_t len, const uint8_t* iv);
-void aes256_cbc_decrypt(uint8_t* data, size_t len, const uint8_t* iv); // na razie pusta
+void aes256_cbc_decrypt(uint8_t* data, size_t len, const uint8_t* iv);
 
 #ifdef __cplusplus
 }
