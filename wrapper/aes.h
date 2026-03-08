e62bbc250fa9198560321ec85651e3a6c7ec2faf
motowrotka <motowrotka@gmail.com>
2026-03-08 15:35:09 +0100

    Add C++ compatibility to aes.h header

--- wrapper/aes.h
+++ wrapper/aes.h
@@ -2,9 +2,17 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void aes256_init(const uint8_t* key);
 void aes256_encrypt_block(uint8_t* block);
 void aes256_decrypt_block(uint8_t* block);
 
 void aes256_cbc_encrypt(uint8_t* data, size_t len, const uint8_t* iv);
 void aes256_cbc_decrypt(uint8_t* data, size_t len, const uint8_t* iv);
+
+#ifdef __cplusplus
+}
+#endif
