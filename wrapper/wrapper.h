14ac6f580765bb69b4836c0b17548fcc18f55dcc
motowrotka <motowrotka@gmail.com>
2026-03-08 17:35:34 +0100

    Update wrapper.h

--- wrapper/wrapper.h
+++ wrapper/wrapper.h
@@ -1,18 +1,12 @@
 #pragma once
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-__declspec(dllexport)
-int EncryptAndCompress(const char* inFile,
-                       const char* outFile,
-                       const char* password);
-
-__declspec(dllexport)
-int DecryptAndDecompress(const char* inFile,
-                         const char* outFolder,
-                         const char* password);
+__declspec(dllexport) int EncryptAndCompress(const char* inFile, const char* outFile, const char* password);
+__declspec(dllexport) int DecryptAndDecompress(const char* inFile, const char* outFolder, const char* password);
 
 #ifdef __cplusplus
 }
