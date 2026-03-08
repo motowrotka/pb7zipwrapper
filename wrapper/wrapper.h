893778c18ebf5b48b70f8a947c59d97e7b893418
motowrotka <motowrotka@gmail.com>
2026-03-08 15:04:25 +0100

    Rename compression and encryption functions

--- wrapper/wrapper.h
+++ wrapper/wrapper.h
@@ -5,14 +5,14 @@ extern "C" {
 #endif
 
 __declspec(dllexport)
-int CompressAndEncryptFile(const char* inFile,
-                           const char* outFile,
-                           const char* password);
+int EncryptAndCompress(const char* inFile,
+                       const char* outFile,
+                       const char* password);
 
 __declspec(dllexport)
-int DecompressAndDecryptFile(const char* archiveFile,
-                             const char* outFolder,
-                             const char* password);
+int DecryptAndDecompress(const char* inFile,
+                         const char* outFolder,
+                         const char* password);
 
 #ifdef __cplusplus
 }
